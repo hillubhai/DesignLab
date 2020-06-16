eac46f9e3abdc47ec8072b4248f12d36244898da
hillubhai <hillolbarman@yahoo.com>
2020-06-16 09:37:55 +0530

    Added Dash, Pleage, List Needs

--- app/src/main/java/com/techsquad/ServerURL.java
+++ app/src/main/java/com/techsquad/ServerURL.java
@@ -1,7 +1,7 @@
 package com.techsquad;
 
 final class ServerURL {
-    private static final String BASE_URL = "https://designlabstuff.pythonanywhere.com";
+    private static final String BASE_URL = "http://designlabstuff.pythonanywhere.com";
     static final String LOGIN_URL = BASE_URL + "/token/login";
     static final String LOGOUT_URL = BASE_URL + "/token/remove";
     static final String PHONE_CHECK_URL = BASE_URL + "/token/exists";
@@ -9,9 +9,9 @@ final class ServerURL {
     static final String REFRESH_TOKEN_URL = BASE_URL + "/";
     static final String CUSTOMER_REGISTER_URL = BASE_URL + "/api/register_cust";
 
-    static final String SEARCH_URL = BASE_URL + "/";
-    static final String DRIVER_BOOK_URL = BASE_URL + "/";
-    static final String PAST_RIDES_URL = BASE_URL + "/";
+    static final String NEEDS_URL = BASE_URL + "/api/needs";
+    static final String DONATIONS_URL = BASE_URL + "/api/donations";
+    static final String DONATE_URL = BASE_URL + "/api/donate";
     static final String UPCOMING_RIDES_URL = BASE_URL + "/";
     static final String ONGOING_RIDE = BASE_URL + "/";
     static final String CUSTOMER_CANCEL_BOOKING_URL = BASE_URL + "/";
@@ -20,7 +20,7 @@ final class ServerURL {
     private static final String REVERSE_GEOCODE_REQUEST = "https://maps.googleapis.com/maps/api/geocode/json?&latlng=";
 
     static String getMapsAppURL(String lat, String lon) {
-        return "https://maps.google.com/maps?daddr=" + lat + "," + lon;
+        return "http://maps.google.com/maps?daddr=" + lat + "," + lon;
     }
 
     static String getRevGeoURL(String s) {
